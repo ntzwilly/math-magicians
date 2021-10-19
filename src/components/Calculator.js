68801bb6bb4834cedd3d8e76d536bf2ec7779bdd
ntzwilly <ntzwilly@gmail.com>
2021-10-19 16:31:56 +0200

    Fix linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,7 +1,11 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
 export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="container">
