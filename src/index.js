49b38276e64fefd53a1a03e7e4ee771f56d3b826
ntzwilly <ntzwilly@gmail.com>
2021-10-19 18:32:21 +0200

    Make required changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './components/Calculator';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
