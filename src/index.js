737866f936d139ff5e0a0e601929d1369cedd1ab
ntzwilly <ntzwilly@gmail.com>
2021-10-19 16:46:22 +0200

    Import Calculator.js in App.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Calculator from './components/Calculator';
-
-class App extends React.Component {
-  render() {
-    return (
-      <Calculator />
-    );
-  }
-}
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
