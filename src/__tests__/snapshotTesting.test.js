ac1460577b934be2bec9c5c149c14d661add08f9
SamanAtashi <milady00126@gmail.com>
2021-10-27 16:06:45 +0300

    Fix linters

--- src/__tests__/snapshotTesting.test.js
+++ src/__tests__/snapshotTesting.test.js
@@ -1,8 +1,8 @@
-import renderer from "react-test-renderer";
-import Calculator from "../components/Calculator";
+import renderer from 'react-test-renderer';
+import Calculator from '../components/Calculator';
 
-describe("Snapshot testing components", () => {
-  it("Calculator snapshot", () => {
+describe('Snapshot testing components', () => {
+  it('Calculator snapshot', () => {
     const tree = renderer.create(<Calculator />).toJSON();
     expect(tree).toMatchSnapshot();
   });
