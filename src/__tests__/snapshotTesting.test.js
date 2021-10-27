09e5983bb9e75bb6d2ed8710230ec5223b774402
SamanAtashi <milady00126@gmail.com>
2021-10-27 16:09:50 +0300

    Add snapshotTesting to Home

--- src/__tests__/snapshotTesting.test.js
+++ src/__tests__/snapshotTesting.test.js
@@ -1,9 +1,15 @@
 import renderer from 'react-test-renderer';
 import Calculator from '../components/Calculator';
+import Home from '../components/Home';
 
 describe('Snapshot testing components', () => {
   it('Calculator snapshot', () => {
     const tree = renderer.create(<Calculator />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('Home snapshot', () => {
+    const tree = renderer.create(<Home />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
